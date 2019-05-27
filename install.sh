8c466d0a465a5939da97a53ddc486d8593e4abec
amaya <mail@sapphire.in.net>
2019-05-27 15:32:20 +0900

    :bug: Fix the install script

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 : ${DIR:='/usr/local/bin'}
 [ "$(ls -ld ${DIR} | cut -d' ' -f3)" != "${USER}" ] && SUDO='sudo' || SUDO=''
 for cmd in anyrc sshrc dockerrc kubectlrc surc; do
-  ${SUDO} curl -sS "https://github.com/amaya382/anyrc/raw/master/${cmd}" -o "${DIR}/${cmd}"
+  ${SUDO} curl -sSL "https://github.com/amaya382/anyrc/raw/master/${cmd}" -o "${DIR}/${cmd}"
   ${SUDO} chmod +x "${DIR}/${cmd}"
 done
 
-curl -sS https://github.com/amaya382/anyrc/raw/master/example/.anyrc -o "${HOME}/.anyrc"
+curl -sSL https://github.com/amaya382/anyrc/raw/master/example/.anyrc -o "${HOME}/.anyrc"
 mkdir -p "${HOME}/.anyrc.d"
 
 cat << EOS
