37e22aa7e1a0e7b8e483de19964b49c0f29f6a54
amaya <mail@sapphire.in.net>
2020-01-27 18:28:12 +0900

    :rocket: Update install script

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 : ${DIR:='/usr/local/bin'}
 [ "$(ls -ld ${DIR} | cut -d' ' -f3)" != "${USER}" ] && SUDO='sudo' || SUDO=''
-echo "Installing anyrc commands into ${DIR}..."
+echo "Installing anyrc commands into ${DIR} ..."
 for cmd in dockrc kuberc dockerrunrc dockerexecrc kubectlexecrc; do
-  ${SUDO} rm "${DIR}/${cmd}" # Remove old commands
+  ${SUDO} rm "${DIR}/${cmd}" &> /dev/null # Remove old commands
 done
 for cmd in anyrc sshrc dockerrc kubectlrc surc; do
   ${SUDO} curl -sSL "https://github.com/amaya382/anyrc/raw/master/${cmd}" -o "${DIR}/${cmd}"
@@ -13,23 +13,23 @@ done
 
 if [ -e "${HOME}/.anyrc" ]; then
   if [ ! -z "${FORCE:+_}" ]; then
-    echo "Override ${HOME}/.anyrc"
+    echo "Overriding ${HOME}/.anyrc"
   else
     SKIP_DANYRC=1
-    echo "Skip installing .anyrc (If you want to install, install again w/ FORCE=1)"
+    echo "Skipping installing .anyrc (If you want to install default .anyrc, install again w/ FORCE=1)"
   fi
 fi
 
 if [ -z "${SKIP_DANYRC:+_}" ]; then
-  echo "Installing ${HOME}/.anyrc..."
+  echo "Installing ${HOME}/.anyrc ..."
   curl -sSL https://github.com/amaya382/anyrc/raw/master/example/.anyrc \
     -o ~/.anyrc
 fi
-echo "Creating ${HOME}/.anyrc.d..."
+echo "Creating ${HOME}/.anyrc.d ..."
 mkdir -p "${HOME}/.anyrc.d"
 
 cat << EOS
 anyrc was successfully installed!
-Put your dotfiles into ${HOME}/.anyrc.d. Symlinks are allowed.
+Put your dotfiles into ${HOME}/.anyrc.d . Symlinks are allowed.
 EOS
 
